01584556773a1363834f6f2355d0745a98d1c31b
Danil <danilsolom@mail.ru>
2020-10-13 19:01:36 +1000

    Add   * Standart interface   * Media query   * Mobile version

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,15 +3,18 @@ const router = Router();
 
 router.get("/:name", (req, res) => {
 	res.status(200);
+	console.log();
 	res.render("index", {
 		name: req.params.name,
+		reason: req.query.reason,
 	});
 });
 
 router.get("/", (req, res) => {
 	res.status(200);
 	res.render("index", {
-		name: "тебя, мой друг",
+		name: "Тебя, мой друг",
+		reason: "По приколу",
 	});
 });
 
